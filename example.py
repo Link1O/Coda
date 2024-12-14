35407bf4d295c9de8eaa86d03f25c8ce74194b24
Link <75633129+Link1O@users.noreply.github.com>
2024-12-14 21:00:21 +0100

    changed the command naming

--- example.py
+++ example.py
@@ -1,17 +1,15 @@
 from Coda.core.http import WebSocket_Handler
 from Coda.core.handler_base import handler_base
 from Coda.core.embed_base import embed_base
-from ..core.constants import *
+from Coda.core.constants import *
 import asyncio
 bot = WebSocket_Handler("your-token", intents=intents_base.ALL, prefix="h!", debug=True)
 async def main(): await bot.connect()
-@bot.command("do")
+@bot.command("say")
 async def cmd(handler: handler_base, value):
-    print(value)
-    await handler.reply("...")
+    await handler.reply(value)
 @bot.on_ready
-async def ss():
-    print("hello")
+async def on_ready_event():
     await bot.change_presence(presence_status_base.DND, "hello", type=presence_type_base.PLAYING)
 loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
