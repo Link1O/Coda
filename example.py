696b4cc8c559dc9d297378bac0244d9332f5299e
Link <75633129+Link1O@users.noreply.github.com>
2025-08-19 16:30:26 +0000

    example changed

--- example.py
+++ example.py
@@ -3,21 +3,27 @@ from Coda.core.handler_base import handler_base
 from Coda.core.constants import *
 import asyncio
 
-shard_manager = ShardManager("your-token", intents=intents_base.ALL, prefix="h!", shard_count=2, debug=True)
+shard_manager = ShardManager(
+    "MTE1NTkwMTM1NDAzMjc1ODk0Nw.G4TCrP.KL-Kj7VwwiI7LfeLlAU4vbLgLF_2E6lJ7B9IuE",
+    intents=intents_base.ALL,
+    prefix="h!",
+    shard_count=3,
+    debug=True
+)
 
 async def main():
-    await shard_manager.start()
-
-# Bind commands and events to all shards
-for shard in shard_manager.shards:
-    @shard.command("say")
-    async def cmd(handler: handler_base, value):
-        await handler.reply(value)
-
-    @shard.on_ready
-    async def on_ready_event():
-        await shard.change_presence(presence_status_base.DND, f"Shard {shard.shard_id}", type=presence_type_base.PLAYING)
-
+    await shard_manager.register()
+    def bind_handlers(shard):
+        @shard.on_ready
+        async def on_ready_event():
+            print(shard.shard_id)
+            await shard.change_presence(presence_status_base.DND, f"shard no: {shard.shard_id}")
+        @shard.command("shrd")
+        async def say(ctx: handler_base):
+            await ctx.reply(content=str(shard.shard_id))
+    for shard in shard_manager.shards:
+        bind_handlers(shard)
+    await shard_manager.connect()
 loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
 loop.run_forever()
