5bcbcacb335c02505a89e4f01e75de7f48a33633
Link <75633129+Link1O@users.noreply.github.com>
2025-08-20 02:17:40 +0000

    sharding is now in cython for better iteration speed

---
+++ Coda/core/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+from Cython.Build import cythonize
+setup(
+    name="ShardManagerModule",
+    ext_modules=cythonize("sharding.pyx", language_level=3),
+    zip_safe=False,
+)
