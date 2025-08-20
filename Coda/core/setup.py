a674622e3fabdcd32d550aef3561dcedc66f19a0
Link <75633129+Link1O@users.noreply.github.com>
2025-08-20 17:13:33 +0000

    followup

--- Coda/core/setup.py
+++
@@ -1,9 +0,0 @@
-# to build run: python setup.py build_ext --inplace
-
-from setuptools import setup
-from Cython.Build import cythonize
-setup(
-    name="ShardManagerModule",
-    ext_modules=cythonize("sharding.pyx", language_level=3),
-    zip_safe=False,
-)
