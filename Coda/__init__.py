d65dfd92fdacdff6f7038e06aafc3128f28cd755
Link1O <elhamradriss@gmail.com>
2025-12-12 15:18:10 +0100

    refactor: binaries path placement

--- Coda/__init__.py
+++ Coda/__init__.py
@@ -2,4 +2,4 @@ from Coda._core.ws import Client
 from Coda.sharding import ShardedClient
 from Coda._core.handlers import *
 from Coda._core.constants import *
-from Coda.tools import DynamicDictManager
+from Coda.utils.tools import DynamicDictManager
